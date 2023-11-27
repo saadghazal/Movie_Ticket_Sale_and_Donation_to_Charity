6484c2ec5e9682f71cbf2951db1238b497b8ac7b
saadghazal <saad.m.ghazal@icloud.com>
2023-11-27 20:25:36 +0300

    build the required problem identifiers

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.print("Enter Movie Name: ");
+        String movieName = console.next(); // get the movie name from the user
+
+        System.out.println();
+        System.out.print("Enter Adult Ticket Price: ");
+
+        double adultTicketPrice = console.nextDouble(); // get the adult ticket price from the user
+        System.out.println();
+        System.out.print("Enter Child Ticket Price: ");
+
+        double childTicketPrice = console.nextDouble(); // get the child ticket price from the user
+        System.out.println();
+        System.out.print("Enter Number Of Adult Tickets Sold: ");
+
+        int numberOfAdultTicketsSold = console.nextInt(); // get the number of sold tickets (adult type)
+        System.out.println();
+        System.out.print("Enter Number Of Child Tickets Sold: ");
+
+        int numberOfChildTicketsSold = console.nextInt(); // get the number of sold tickets (child type)
+        System.out.println();
+        System.out.print("Enter Percentage Of Charity Donations: ");
+
+        double percentageOfCharityDonation = console.nextDouble(); // get the percentage of total sold tickets amount for charity dnations
+
+
+
+
 
     }
 }
